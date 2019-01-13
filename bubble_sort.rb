c1797e69580ca3e61c585db92d0216b714e34485
Gabe Horvat <realgabrielhorvat@gmail.com>
2019-01-13 11:32:34 +0100

    add if array empty return straight away

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,25 +1,27 @@
 # create bubble sort in ruby
 def bubble_sort(array)
+  # total number of elements in our array
   n = array.length
-
-  loop do
-
-    # initialize swapped boolean as false
-    swapped = false
-    # iterate through given array until end of arrray eg array - 1
-    (n - 1).times do |i|
-      if array[i] > array[i + 1] # if ith element in given array is smaller than next array ie array[i + 1]
-        array[i], array[i + 1] = array[i + 1], array[i] # swap array[i] with element right next to it
-        # set swapped boolean to true since we just swapped adjacent array elements
-        swapped = true
+  if n < 1 # if our array is empty or only contains one element return it straight away so as to not do unneccessary work
+    array
+  else
+    # sort array
+    loop do
+      # initialize swapped boolean as false
+      swapped = false
+      # iterate through given array until end of arrray eg array - 1
+      (n - 1).times do |i|
+        if array[i] > array[i + 1] # if ith element in given array is smaller than next array ie array[i + 1]
+          array[i], array[i + 1] = array[i + 1], array[i] # swap array[i] with element right next to it
+          # set swapped boolean to true since we just swapped adjacent array elements
+          swapped = true
+        end
       end
+      break if swapped != true # break loop once itereated through entire array else you get infinite loop and terminal crashes
     end
-
-    break if swapped != true # break loop once itereated through entire array else you get infinite loop and terminal crashes
+    # return sorted array
+    array
   end
-
-  # return sorted array
-  array
 end
 
 # test method
